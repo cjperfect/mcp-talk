0a08850ed0bf6a2242546c5a4f5aa814c0d91a42
outsourcer-07 <cjperfect@qq.com>
2025-04-14 19:36:20 +0800

    update

---
+++ docs/assets/md-DYCabAj3.js
@@ -0,0 +1,7 @@
+import{aJ as c,b as m,o,w as u,g as t,ah as i,ad as l,f as n,v as d,x as g,S as r}from"./modules/vue-uvfpF4v6.js";import{I as _}from"./slidev/default-Ce1mLiUK.js";import{u as f,f as P}from"./slidev/context-Dwb5JsGy.js";import"./index-B3J_GJ9H.js";import"./modules/shiki-DxUXYpQs.js";const v="/mcp-talk/assets/ai-mcp-WJnAALPZ.png",C="/mcp-talk/imgs/mcp-github.png",b={class:"font-size-[14px]"},x={class:"mt-2"},k={class:"grid grid-cols-2 gap-2"},D={__name:"slides.md__slidev_7",setup(M){const{$clicksContext:a,$frontmatter:p}=f();return a.setup(),(A,s)=>{const e=c("click");return o(),m(_,d(g(r(P)(r(p),6))),{default:u(()=>[s[3]||(s[3]=t("h1",null,"MCP到底是什么",-1)),s[4]||(s[4]=t("p",null,[l("本质上，MCP解决了AI模型的一大痛点："),t("strong",null,"它们通常只知道训练时学到的东西，很难获取到实时信息或者执行实际操作。")],-1)),i((o(),n("div",b,[s[1]||(s[1]=l(" 假设你是老板，AI是你的新员工。这位员工刚刚毕业，经验不足，知识有限，你交代的任务，他可能缺乏完成任务所需的工具或者信息。这时，MCP就像是一个工具箱或者仓库。 ")),i((o(),n("div",x,s[0]||(s[0]=[t("img",{class:"m-auto",src:v,width:"80%"},null,-1)]))),[[e]])])),[[e]]),i((o(),n("div",k,s[2]||(s[2]=[t("div",{class:"font-size-[14px]"},[t("pre",null,[t("code",null,`<span><b class="color-blue mb-2">举例：</b>假设你想让AI帮你管理github项目:</span>
+
+1. 你设置了一个MCP服务器，连接到github API
+2. 当你用cline、claude(MCP客户端)等工具去访问“最近有哪些issue”
+3. MCP客户端通过MCP从github获取最新的数据
+4. 然后基于实时信息给你分析报告
+`)])],-1),t("img",{src:C,width:"80%",class:"mt-2"},null,-1)]))),[[e]])]),_:1},16)}}};export{D as default};
